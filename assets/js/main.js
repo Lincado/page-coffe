27324127cba456576e6f79b292f02f76df100887
Leo Alves <leoalves0698@gmail.com>
2023-02-16 18:29:03 -0300

    criando responsividade mobile e alinhamento

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,13 @@
 const p = document.querySelector(".inner-p")
 p.innerText = "<Great Code/>"
+
+const btnMenuMobile = document.querySelector("#btn-menu-mobile")
+
+function toggleMenu (event) {
+    
+    const nav = document.getElementById("nav")
+    nav.classList.toggle("active")
+
+}
+
+btnMenuMobile.addEventListener("click", toggleMenu)
