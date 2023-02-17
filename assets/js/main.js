678ca0a8c68f6a8842a2ee9171cf46d2b3999d67
Leo Alves <leoalves0698@gmail.com>
2023-02-17 17:47:01 -0300

    responsividade adicionada e menu retrátil

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,12 +2,16 @@ const p = document.querySelector(".inner-p")
 p.innerText = "<Great Code/>"
 
 const btnMenuMobile = document.querySelector("#btn-menu-mobile")
+btnMenuMobile.innerHTML = `<img src="./assets/img/menu-buguer-open.svg" alt="close menu">`
 
 function toggleMenu (event) {
-    
     const nav = document.getElementById("nav")
-    nav.classList.toggle("active")
-
+    const active =nav.classList.toggle("active") 
+    if(active === true) {
+    btnMenuMobile.innerHTML = `<img src="./assets/img/menu-buguer-close.svg" alt="close menu">`
+    } else {
+        btnMenuMobile.innerHTML = `<img src="./assets/img/menu-buguer-open.svg" alt="open menu">`
+    }
 }
 
 btnMenuMobile.addEventListener("click", toggleMenu)
